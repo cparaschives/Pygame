e7a3c605c9304bf9090df763848d72934ec45530
pfb23 <pfb23@info23.cshl.edu>
2023-10-30 13:19:39 -0400

    changed file

--- pygame_exercises/pygame_character_def_v4.py
+++ pygame_exercises/pygame_character_def_v4.py
@@ -11,31 +11,31 @@ pygame.display.set_caption("Microbial_Mayhem")
 
 isRunning = True
 
-background=pygame.image.load("/Users/pfb23/Desktop/bacterium_images/background2.png")
+background=pygame.image.load("/Users/pfb23/Desktop/bacterium_images/background4.png")
 background=pygame.transform.scale(background,(width*1,height*1))
 
 #Loading microbe_1
 player_1 = pygame.image.load("/Users/pfb23/Desktop/bacterium_images/cropped/M3-transparent.png").convert_alpha()
 player_2 = pygame.image.load("/Users/pfb23/Desktop/bacterium_images/cropped/M4-transparent.png").convert_alpha()
    
-w = player_1.get_width()
-h = player_1.get_height()
+w1 = player_1.get_width()
+h1 = player_1.get_height()
 
-w = player_2.get_width()
-h = player_2.get_height()
+w2 = player_2.get_width()
+h2 = player_2.get_height()
 
-player_1_size=pygame.transform.scale(player_1, (w*0.5, h*0.5))
+player_1_size=pygame.transform.scale(player_1, (w1*0.5, h1*0.5))
 player_1_rotate=pygame.transform.rotate(player_1_size, -10)
 
-player_2_size=pygame.transform.scale(player_2, (w*0.5, h*0.5))
+player_2_size=pygame.transform.scale(player_2, (w2*0.5, h2*0.5))
 player_2_rotate=pygame.transform.rotate(player_2_size, -10)
-
+ 
 #Specifying the X and Y Coordinate
 player1_X = 55 
-player1_Y = 400 
+player1_Y = 80 
 
 player2_X = 700 
-player2_Y = 400 
+player2_Y = 80 
 
 X1_change = 0 
 Y1_change = 0 
@@ -43,8 +43,21 @@ Y1_change = 0
 X2_change = 0
 Y2_change = 0 
 
+#def collision_has_occured():
+    
+#  player_1_top_right_x = player1_X + w1
+#  player_1_top_right_y = player1_Y
+#  player_1_bottom_right_x = player1_X + w1
+#  player_1_bottom_right_y = player1_Y - h1 
+
+    
+#   return False
+
+
+
+
 while(isRunning ==True):
-    screen.fill((0,0,0))
+    screen.fill((255,255,255))
     screen.blit(background,(2.5,2))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
